22e6c6bd04fd22e9fccc6f49e1c2d98b4901eca7
= <=>
2024-05-07 10:54:17 +0200

    added Java examples

---
+++ AwtExample.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class AwtExample extends Frame {
+  public AwtExample() {
+    Button btn = new Button("Button");
+    btn.setBounds(50, 50, 50, 50);
+    add(btn);
+    setSize(150, 150);
+    setTitle("This is my First AWT example");
+    setLayout(new FlowLayout());
+    setVisible(true);
+    addWindowListener(new WindowAdapter() {
+        public void windowClosing(WindowEvent we) {
+            dispose();
+        }
+    });
+  }
+
+  public static void main(String args[]){
+    new AwtExample();
+  }
+}
